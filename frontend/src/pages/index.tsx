117f2b7d260ec1fc5b7cb99e494effa5823da3e4
Xian Zheng <seeinplay@gmail.com>
2023-11-08 13:54:34 +0300

    working zuauth sign in

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,3 @@
-"use client";
 import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
@@ -59,7 +58,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-12 pb-32'>
       <Head>
-        <title>ZuAuth Example</title>
+        <title>DeSci Logger</title>
         <link rel='icon' type='image/x-icon' href='/favicon.ico' />
       </Head>
       <div className='max-w-xl w-full'>
@@ -74,42 +73,6 @@ export default function Home() {
 
         <h1 className='my-8 text-2xl font-semibold text-center'>Login</h1>
 
-        <p className='text-justify'>
-          This demo illustrates how the{" "}
-          <a
-            className='text-blue-600 visited:text-purple-600'
-            href='http://npmjs.com/package/zuauth'
-            target='_blank'
-          >
-            <b>zuauth</b>
-          </a>{" "}
-          NPM package can be used in a simple authentication system with{" "}
-          <a
-            className='text-blue-600 visited:text-purple-600'
-            href='https://nextjs.org/'
-            target='_blank'
-          >
-            NextJS
-          </a>{" "}
-          and{" "}
-          <a
-            className='text-blue-600 visited:text-purple-600'
-            href='https://github.com/vvo/iron-session'
-            target='_blank'
-          >
-            IronSession
-          </a>
-          . Check the{" "}
-          <a
-            className='text-blue-600 visited:text-purple-600'
-            href='https://github.com/cedoor/zuauth#readme'
-            target='_blank'
-          >
-            README file
-          </a>{" "}
-          in the repository to learn more about the code.
-        </p>
-
         <div className='my-8 text-center'>
           <button
             className='bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded'
@@ -123,7 +86,7 @@ export default function Home() {
 
         <div className='mt-10 text-center'>
           <a
-            href='https://github.com/cedoor/zuauth'
+            href='https://github.com/seeincodes/zuhack-istanbul'
             className='underline'
             target='_blank'
           >
